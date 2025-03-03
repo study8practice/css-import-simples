d208f0bf1ee92f88a2181610bbae8af752528218
study8practice <156129782+study8practice@users.noreply.github.com>
2025-03-03 18:57:52 -0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <p className="text">Meu site</p>
+    </div>
+  );
+};
+
+
+export default App;
